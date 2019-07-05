deec08789f1b0e9bc9bbbeac566a57d59ba27047
selman-keskin <32268443+selman-keskin@users.noreply.github.com>
2019-07-06 01:29:01 +0300

    reduce your search space by close to 50%
    99*91=9009
    993*913=906609
    9999*9901=99000099
    99793*99041=1293663921
    977257*903156=2147227412

--- src/palindromic/Main.java
+++ src/palindromic/Main.java
@@ -4,33 +4,18 @@ public class Main {
 	
 	public static boolean isPal(long num) 
 	{
-		String numStr = Long.toString(num);
-		String rNumStr = "";
-		
-		boolean result = false;
-		
-		for (int i = numStr.length() - 1; i >= 0 ; --i)
-			rNumStr += numStr.charAt(i);
-		
-		//System.out.println(numStr + "," + rNumStr);
-		try 
-		{
-			if (Long.parseLong(numStr) == Long.parseLong(rNumStr))
-				result = true;
-			else  
-				result = false;
-		}catch (NumberFormatException e) {
-            //System.err.println("Unable to format. " + e);
-        }
-		return result;
-		
+	    String numStr = Long.toString(num);
+	    StringBuilder sb = new StringBuilder(numStr);
+	    String rNumStr = sb.reverse().toString();
+
+	    return numStr.equals(rNumStr);	
 	}
 	
 	public static void calcPal(int rangeMin, int rangeMax)
 	{
 		long maxp = 0, maxq = 0, maxP = 0;
-        for (int p = rangeMax; p > rangeMin; p--)
-	        for (int q = rangeMax; q > rangeMin; q--)
+        for (int p = rangeMax; p >= rangeMin; p--)
+	        for (int q = p; q >= rangeMin; q--)
 	        {
 	            long P = p * q;
 	            if (P % 11 != 0)
@@ -47,11 +32,11 @@ public class Main {
 	
 	public static void main(String[] args) 
 	{
-		calcPal(10, 99);
-		calcPal(100, 999);
-		calcPal(1000, 9999);
-		calcPal(10000, 99999);
-		calcPal(100000, 999999);
+		calcPal(90, 99);
+		calcPal(900, 999);
+		calcPal(9000, 9999);
+		calcPal(90000, 99999);
+		calcPal(900000, 999999);
 	}
 
 }
